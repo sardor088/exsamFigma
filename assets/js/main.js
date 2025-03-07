dce48e53748a785ba93a9d05891e491f4912f0fc
sardor088 <sardorhakimov012@gmail.com>
2025-03-07 15:43:34 +0300

    homePage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,57 @@
-// const list = document.querySelector('.teach-list');
-// const API = "https://api.39ortomekteb.info/api/teachers";
+const API = "https://api.39ortomekteb.info/api/teachers";
+const apiEvent = "https://api.39ortomekteb.info/api/blog";
+const galleryAPI = "https://api.39ortomekteb.info/api/gallery";
+const list = document.querySelector('.teach_api');
+const evtList = document.querySelector('.event_list');
+const galleryItem = document.querySelector('.gallery_list');
 
-// fetch(API)
-//     .then((res) => res.json())  
-//     .then((data) => {
-//         if (data.success) {
-//             data.data.forEach(teacher => {  
-//                 const listItem = document.createElement('li');
-//                 listItem.textContent = `${teacher} (${teacher.type})`;  // Отображаем имя и должность
-//                 list.appendChild(listItem);  
-//             });
-//         } else {
-//             console.log('Ошибка: Не удалось получить данные');
-//         }
-//         console.log(data);
-        
-//     })
-//     .catch((error) => {
-//         console.error("Ошибка при загрузке данных:", error);
-//     });
+// Fetch teachers data
+fetch(API)
+    .then((res) => res.json())
+    .then((teachers) => {
+        teachers = teachers.data;
+        teachers.forEach((item) => {
+            const li = document.createElement("li");
+            li.classList.add('teach_li');
+            li.innerHTML = `
+                <img class="teach_img" src="${item.image}" alt="">
+                <p class="teach_name">${item.full_name}</p> 
+                <p class="teach_name">${item.subject}</p> 
+            `;
+            console.log(item);
+            list.appendChild(li);
+        });
+    })
+    .catch((err) => console.log("ошибка", err));
+
+fetch(apiEvent)
+    .then((res) => res.json())
+    .then((events) => {
+        events = events.data;
+        events.forEach((item) => {
+            const liEvt = document.createElement('li');
+            liEvt.classList.add('eventList');
+            let wrap = JSON.parse(`${item.body}`); 
+            liEvt.innerHTML = `
+                <img class="event_img" src="${item.image}" alt=""> 
+                <p class="event_date">${item.createdAt}</p>
+                <p class="event_name">${item.title}</p> 
+            `;
+            evtList.appendChild(liEvt);
+        });
+    })
+    .catch((err) => console.log("ошибка", err));
+
+fetch(galleryAPI)
+    .then((res) => res.json())  
+    .then((data) => {
+        data.forEach((item) => {
+            const galleryList = document.createElement('li');
+            galleryList.classList.add('gallery_list');
+            galleryList.innerHTML = `
+                <img class="event_img" src="${item.image}" alt="">                  <p class="event_date">${item.createdAt}</p>
+            `;
+            galleryItem.appendChild(galleryList);
+        });
+    })
+    .catch((err) => console.log("ошибка", err));
